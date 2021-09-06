03720a091553ac372bebdab133208eb3ccd92263
Marcello Lima <araujodba@gmail.com>
2021-09-06 03:11:20 -0300

    Initial Commit

---
+++ Lesson 13 Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lesson 13 Challenge
+//
+//  Created by Marcello Lima on 06/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lesson 13 Challenge/Lesson_13_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_13_ChallengeApp.swift
+//  Lesson 13 Challenge
+//
+//  Created by Marcello Lima on 06/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_13_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
